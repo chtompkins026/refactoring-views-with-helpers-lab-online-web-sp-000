adbb9f3dfedeaf134d75e6709bd2c67c815c0487
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-17 20:39:25 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,9 @@
 module ArtistsHelper
   
-  def display_artist 
+  def display_artist(song) 
+    if song.artist.present? 
+    else 
+      link_to "Add Artist", edit_song_path(song)
   end 
   
 end
